a5fa25341d9380646d7bb3f4b66230a1c698801c
weilok2021 <weilok629@gmail.com>
2025-09-09 22:46:58 +0800

    Using arr.splice method in for loops is tricky
    As it changes the arr.length while we are running for loops
    Solved this problem by decrementing loop counter after calling splice to remove element

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...elems) {
+    for (let i = 0; i < arr.length; i++) {
+        // IF the argument is found in the array, store the index
+        for (elem of elems) {
+            if (arr[i] === elem) {
+                console.log(arr[i]);
+                // REMOVE one element at ith position
+                arr.splice(i, 1);
+                // after we remove one element, 
+                // the arr.length has decremented by 1, so next element will move to current position
+                // Thus we have to decrement loop counter so that in next iteration,
+                // the next element will be checked with current index
+                i--;
+            }
+        }
+    }
+
+    return arr;
 };
+
+const arr = [1, 2, 3, 4];
+console.log(removeFromArray(arr, 3));
+console.log(arr);
 
 // Do not edit below this line
 module.exports = removeFromArray;
