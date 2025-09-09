b73af5890e95a9c728e98c7cdcda8757a7eaa921
weilok2021 <weilok629@gmail.com>
2025-09-09 22:06:46 +0800

    repeatString is done via string concatenation and for loops

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(text, time) {
+    if (time < 0) 
+        return "ERROR";
+    let repeat = "";
+    for (let i = 0; i < time; i++) {
+        repeat += text;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
