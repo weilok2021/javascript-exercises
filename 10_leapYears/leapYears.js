5f1d2bb3b28134e03c9cae25f98cbec90e3a65b1
weilok2021 <weilok629@gmail.com>
2025-09-10 13:38:12 +0800

    leapYear done using nested if blocks

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,30 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // IF year is divisible by 4
+    //     IF year is divisible by 100 
+    //         IF year is divisible by 400
+    //             THEN it is leap year
+    //         ELSE year is only divisible by 100
+    //             THEN it is not leap year
+    //     THEN it is leap year
+    // ELSE
+    //     THEN it is not leap year
+    // ENDIF
 
+    
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
