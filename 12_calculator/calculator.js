6b92f06c51dd3feeb5c3a74e5856bd061cde680c
weilok2021 <weilok629@gmail.com>
2025-09-10 17:15:58 +0800

    12_calculator: various functions are done

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const total = arr.reduce((total, num) => total + num, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	const product = arr.reduce((product, num) => product * num, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n < 0) {
+    return "ERROR";
+  }
+  else if (n === 0) {
+    return 1;
+  }
+  else {
+    let fact = 1;
+    while (n >= 1) {
+      fact *= n;
+      n--;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
