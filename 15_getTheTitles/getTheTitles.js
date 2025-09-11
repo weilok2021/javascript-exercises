1902cc89ce81744f2f2c55313ef19f1656cc7dc7
weilok2021 <weilok629@gmail.com>
2025-09-11 12:53:53 +0800

    15. getTheTitles map an array of books to an array of book's title using map method

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    // MAP an array of books to array of book's title
+    // STORE the array of titles into a variable
+    const titles = books.map((book) => book.title);
+    // RETURN the array of titles
+    return titles;
 };
 
 // Do not edit below this line
