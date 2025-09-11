8990c16ef1df37d7148fd0ddeddecb6f92782923
weilok2021 <weilok629@gmail.com>
2025-09-11 12:44:47 +0800

    14. solved fibonacci function using iterative method

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    // This function will return nth number in the fibonacci series
+    // eg. fibonacci(5) = 1, 1, 2, 3, 5
+    if (typeof n === "string")
+        n = parseInt(n);
+    if (n < 0) {
+        return "OOPS";
+    }
+    else if (n == 0 || n == 1) {
+        return n;
+    }
+    let prev1 = 0;
+    let prev2 = 1;
+    let curr;
+    for (let i = 1; i < n; i++) {
+        curr = prev1 + prev2;
+        // set prev1 to be prev2
+        prev1 = prev2;
+        // set prev2 to become curr
+        prev2 = curr;
+    }
+    return curr;
 };
 
+console.log(fibonacci(5));
+console.log(fibonacci(0));  
 // Do not edit below this line
 module.exports = fibonacci;
